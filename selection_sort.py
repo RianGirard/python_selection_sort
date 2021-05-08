fa55522fa7daef97d2f2a0b730c44210a892dc2b
Rian Girard <riangirard@gmail.com>
2021-05-08 00:48:44 -0700

    updated comments

--- selection_sort.py
+++ selection_sort.py
@@ -1,12 +1,11 @@
 arr = [5,9,8,4,3,1,2,6]
 print(arr)
 def selection_sort(lis):
-    for i in range(len(lis)):   # pass through the full array
-        index = i
-        for j in range(i+1, len(lis)):  # pass through the array above the current i-value comparing each of the numbers(j) to the current i-value
-            if lis[index] > lis[j]:
-                index = j       # here we find the index of the new minimum value 
-        # swap the found element (j) with the first element
-        lis[i], lis[index] = lis[index], lis[i]
+    for i in range(len(lis)):   # this loop defines our outer passes through the array, starting with "i=zero"
+        index = i               # this is our "marker" index, a placeholder for the lowest value found
+        for j in range(i+1, len(lis)):  # this loop defines our inner passes through the array, each time "forgetting" the prior digit (i+1)
+            if lis[index] > lis[j]:     # if our marker value > the value j from the inner pass...
+                index = j       # ...then shift the marker to the value j; this always puts the marker value on the lowest "j"
+        lis[i], lis[index] = lis[index], lis[i]     # now that marker is on lowest "j" from inner loop, swap the marker with "i" from outer loop
     print(lis)
 selection_sort(arr)
